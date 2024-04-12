9d46d4ad07aa030de01ca9a75c6c3ce6c5c38168
hemasreebheemireddy <157704506+hemasreebheemireddy@users.noreply.github.com>
2024-04-12 16:05:40 +0530

    #1209 AEB.js
    The vehicle shall perform an emergency break if there is a pedestrian in the activation zone.

---
+++ AEB.js
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Automated Emergency Breaking</h1>
+  <Script> [TASK-1209] The vehicle shall perform an emergency break if there is a pedestrian in the activation zone 
+</script>
+</body>
+</html>
