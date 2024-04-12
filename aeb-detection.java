950ec64a73da2775f284b964cb8e9214c1e1c979
hemasreebheemireddy <157704506+hemasreebheemireddy@users.noreply.github.com>
2024-04-12 16:08:10 +0530

    #1210 aeb-detection.java
    The vehicle shall perform an emergency break if there is a vehicle in the activation zone   depends on this item.

---
+++ aeb-detection.java
@@ -0,0 +1,8 @@
+public class AEBPedestrianDetection {
+    public static void main(String[] args) {
+        System.out.println("Detected pedestrians:");
+        System.out.println("Pedestrian 1");
+        System.out.println("Pedestrian 2");
+        System.out.println("Pedestrian 3");
+    }
+}
